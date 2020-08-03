a243b815216b82ebfddc1d4e944799316e65d1a3
iwasiman <iwasiman@gmail.com>
2020-08-03 15:15:32 +0900

    propTypesの練習

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // propsのテスト
 const Cat = (props) => {
   return (<div>やあ、ミーの名前は{props.name}だにゃー。年は{props.age}だよ！</div>);
 };
 Cat.defaultProps = {
-  age: 1
+  age: 1, 
 };
+Cat.propTypes = {
+  name: PropTypes.string, //小文字開始なのに注意
+  age: PropTypes.number.isRequired, //必須になる
+}
 
 function App() {
   const profiles = [
     {name: "シューちゃん", },
-    {name: "ジェラトーニ", age: "不明"},
+    {name: "ジェラトーニ", age: 3},
+    //{name: 123}, // prop-typesが動くと、DevToolのConsoleに警告が出る
+    //{name: "いちにさん", age: "1"}, // これも警告が出る
+    {name: "エトワール", age: null}, // isRequiredの警告が出る
+    {name: "エトワール2号", age: 2}, // 正常。
   ];
   return (
     <div>
